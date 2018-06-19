da6b67cc14d1840140a9acd43d3ce99231ca750a
Chariotwheel <karl@traumlicht.org>
2018-06-19 23:55:53 +0200

    Add Commentfaces

--- enhance.js
+++ enhance.js
@@ -1,21 +1,47 @@
+var commentfaces = [
+  "firstthinginthemorning",
+  "watashihasdeclined",
+  "killitwithfire",
+  "toradorasalute",
+  "kukuku2"
+];
+
+// https://b.thumbs.redditmedia.com/-H-7U_6ANr6zR_Mvkzbv0hiKzlVdm1vR7G6egC5WICs.css
+// regex: \.md \[href\$="#.*\]
+
+var filteredFaces = [];
+
 var menu = '<div style="" class="md"><form action="" id="commentfaces">';
-menu += '<input type="text" placeholder="search commentfaces"><br /><div style="height:100px;overflow-hidden;">';
-menu += '<div style="width:100%;height:100%;overflow-y:scroll;padding-right:17px;box-sizing:content-box;">';
-menu += '<a href="#firstthinginthemorning" data="firstthinginthemorning" class="addCommentface" rel="nofollow"></a>';
+menu += '<input type="text" id="commentfacesearch" placeholder="search commentfaces"><br /><div style="height:100px;overflow-hidden;">';
+menu += '<div id="commentfacecontainer" style="width:100%;height:100%;overflow-y:scroll;padding-right:17px;box-sizing:content-box;">';
 menu += '</div></div></form></div>';
 
 $( ".usertext-edit" ).prepend( menu );
 
 var classname = document.getElementsByClassName("addCommentface");
 
-for (var i = 0; i < classname.length; i++) {
-    var data = classname[i].getAttribute('data');
-    classname[i].addEventListener('click', addCommentface(data), false);
-}
+document.getElementById("commentfacesearch").addEventListener('keyup', function() {
 
-function addCommentface(data) {
-  var commentcode = 'firstthinginthemorning';
-  var output = '[](#'+commentcode+')';
-  console.log("test");
-  $("textarea").val($('textarea').val()+output);
-}
+  if(this.value.length > 1) {
+
+    var result = "";
+    let filteredFaces = commentfaces.filter(x => x.toLowerCase().includes(this.value));
+
+    filteredFaces.forEach(function(filteredFace) {
+      result += "<a href=\"#"+filteredFace+"\" class=\"addCommentface\" rel=\"nofollow\"></a>";
+    });
+
+    $("#commentfacecontainer").html(result);
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', function() {
+
+          var commentcode = this.getAttribute("href");
+          var output = '[]('+commentcode+')';
+          $("textarea").val($('textarea').val()+output);
+
+        }, false);
+    }
+  }
+
+});
