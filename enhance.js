a8237230c9e0ca429913d1ab87aa194494d16d80
Chariotwheel <karl@traumlicht.org>
2018-06-20 10:28:41 +0200

    Prepare API Call

--- enhance.js
+++ enhance.js
@@ -3,16 +3,32 @@ var commentfaces = [
   "watashihasdeclined",
   "killitwithfire",
   "toradorasalute",
-  "kukuku2"
+  "kukuku2",
+  "akkotears"
 ];
 
 // https://b.thumbs.redditmedia.com/-H-7U_6ANr6zR_Mvkzbv0hiKzlVdm1vR7G6egC5WICs.css
 // regex: \.md \[href\$="#.*\]
 
+var apiurl = 'https://shit-taste.net/api/user/?user=Chariotwheel';
+
+var request = new XMLHttpRequest();
+request.open("POST", apiurl);
+request.setRequestHeader("Content-Type", "application/json");
+request.overrideMimeType("text/plain");
+request.onload = function()
+{
+    var options = JSON.parse(request.responseText);
+    console.log("Response received: " + options[0]);
+    //$(".page-content .avatar").css("max-width","230px");
+    //$(".name").css("color","red");
+};
+request.send();
+
 var filteredFaces = [];
 
 var menu = '<div style="" class="md"><form action="" id="commentfaces">';
-menu += '<input type="text" id="commentfacesearch" placeholder="search commentfaces"><br /><div style="height:100px;overflow-hidden;">';
+menu += '<input type="text" id="commentfacesearch" placeholder="search commentfaces"><br /><div id="commentfacewrapper" style="height:150px;display:none;overflow-hidden;">';
 menu += '<div id="commentfacecontainer" style="width:100%;height:100%;overflow-y:scroll;padding-right:17px;box-sizing:content-box;">';
 menu += '</div></div></form></div>';
 
@@ -22,10 +38,11 @@ var classname = document.getElementsByClassName("addCommentface");
 
 document.getElementById("commentfacesearch").addEventListener('keyup', function() {
 
-  if(this.value.length > 1) {
+  if(this.value.length > 0) {
 
     var result = "";
     let filteredFaces = commentfaces.filter(x => x.toLowerCase().includes(this.value));
+    $("#commentfacewrapper").css("display","inherit");
 
     filteredFaces.forEach(function(filteredFace) {
       result += "<a href=\"#"+filteredFace+"\" class=\"addCommentface\" rel=\"nofollow\"></a>";
